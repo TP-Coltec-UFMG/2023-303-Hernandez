47f7e7d2c5f51ae68c068a09ebb72059932f067f
Gabriela Costa <104565453+gaiascosta@users.noreply.github.com>
2023-08-04 07:36:44 -0300

    Adicionando os itens.

--- Hernandez - Menu/Assets/Scripts/DisplayItemText.cs
+++ Hernandez - Menu/Assets/Scripts/DisplayItemText.cs
@@ -2,27 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DisplayItemText : MonoBehaviour
+public class DisplayInventario : MonoBehaviour
 {
     [SerializeField] Inventario item;
-    [SerializeField] Inventario itemFalso;
     public void OnMouseOver() 
     {
         if(item.activeDrag)
         {
             item.DisplayText(true);
-            Debug.Log("correto");
-        }
-        else if(itemFalso.activeDrag)
-        {
-            itemFalso.DisplayText(true);
-            Debug.Log("errado");
         }
     }
     private void OnMouseExit()
     {
         item.DisplayText(false);
-        itemFalso.DisplayText(false);
     }
 
 
