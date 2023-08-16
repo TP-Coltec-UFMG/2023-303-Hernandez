0c520f1c7211f94f550294fb2bfe4631297bcfc0
Izabela Fonseca <2021951493@teiacoltec.org>
2023-08-15 22:14:14 -0300

    Consertando os fios

--- Hernandez - Menu/Assets/Scripts/Wires.cs
+++ Hernandez - Menu/Assets/Scripts/Wires.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Wires : MonoBehaviour
 {
     Vector3 startPoint;
+    Vector3 startPosition;
     [SerializeField] SpriteRenderer wireEnd;
     // Start is called before the first frame update
     void Start()
     {
         startPoint = transform.parent.position;
+        startPosition = this.transform.position;
     }
 
     // Update is called once per frame
@@ -21,12 +23,36 @@ public class Wires : MonoBehaviour
     private void OnMouseDrag(){
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         newPosition.z = 0;
+        
+        //Pega todos os colisores ao redor
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(newPosition, .2f);
+
+        foreach(Collider2D collider in colliders){
+            //Verifica se não é o próprio colisor
+            if(collider.gameObject != this.gameObject){
+            UpdateWire(collider.transform.position);
+            //retorna para impedir que o outro UpdateWire ocorra
+            return;
+            }
+        }
+
+        
+
+        UpdateWire(newPosition);
+    }
+
+    private void OnMouseUp(){
+        UpdateWire(startPosition);
+
+    }
+
+    void UpdateWire(Vector3 newPosition){
         this.transform.position = newPosition;
 
-        Vector3 direction = startPoint - newPosition;
-        this.transform.right = direction;
+        Vector3 direction = newPosition - startPoint;
+        this.transform.right = direction * transform.lossyScale.x;
 
-        float dist = Vector2.Distance(newPosition, startPoint);
-        wireEnd.size = new Vector2(dist, wireEnd.size.y);
+        float dist = Vector2.Distance(startPoint, newPosition);
+        wireEnd.size = new Vector2((dist+0.1f), wireEnd.size.y);
     }
 }
