2e92e384d331224e3d164b738bd88770cb00fc86
smolinskiJP <2021951450@teiacoltec.org>
2023-08-21 15:54:16 -0300

    Continuação do áudio do PC

--- Hernandez - Menu/Assets/Scripts/AudioComputer.cs
+++ Hernandez - Menu/Assets/Scripts/AudioComputer.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
+using TMPro;
 
 public class AudioComputer : MonoBehaviour
 {
@@ -9,11 +11,28 @@ public class AudioComputer : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject pause;
     [SerializeField] private GameObject play;
+    [SerializeField] private TextMeshProUGUI cronometro;
     private bool ativo = false;
+
+    private int tempo;
+    private int seg;
+    private int min;
+
+    private float segAtual = 0f;
 
     void Start()
     {
         audio = GetComponent<AudioSource>();
+    }
+
+    private void Update() {
+        if(audio.isPlaying){
+            this.tempo = (int)this.audio.time;
+            this.seg = tempo % 60;
+            this.min = (tempo / 60) % 60;
+            if (seg < 10) this.cronometro.text = min.ToString() + ":0" + seg.ToString() + "/0:45";
+            else this.cronometro.text = min.ToString() + ":" + seg.ToString() + "/0:45";
+        }
     }
 
     public void AbrePlayer(){
@@ -21,6 +40,7 @@ public class AudioComputer : MonoBehaviour
             this.player.SetActive(true);
             this.ativo = true;
             this.audio.Play();
+            this.audio.time = segAtual;
 
             this.play.SetActive(false);
             this.pause.SetActive(true);
@@ -38,6 +58,7 @@ public class AudioComputer : MonoBehaviour
         if(audio.isPlaying) return;
         else if (ativo){
             this.audio.Play();
+            this.audio.time = segAtual;
             this.play.SetActive(false);
             this.pause.SetActive(true);
         }
@@ -45,10 +66,16 @@ public class AudioComputer : MonoBehaviour
 
     public void PausaMusica(){
         if(audio.isPlaying){
+            this.segAtual = this.audio.time;
             this.audio.Stop();
 
             this.play.SetActive(true);
             this.pause.SetActive(false);
         } else this.TocaMusica();
+    }
+
+    public void ResetaMusica(){
+        this.segAtual = 0;
+        this.audio.time = segAtual;
     }
 }
