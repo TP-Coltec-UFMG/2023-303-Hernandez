5a82af947d8bb43779b252e507d143293e5aba4b
smolinskiJP <2021951450@teiacoltec.org>
2023-08-22 11:52:15 -0300

    Tela de Bloqueio do Computador

--- Hernandez - Menu/Assets/Scripts/TextoComputador.cs
+++ Hernandez - Menu/Assets/Scripts/TextoComputador.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class TextoComputador : MonoBehaviour
@@ -8,7 +9,11 @@ public class TextoComputador : MonoBehaviour
     [SerializeField] private GameObject painel;
     [SerializeField] private GameObject resposta;
     [SerializeField] private GameObject block;
+    [SerializeField] private GameObject areaDeTrabalho;
+    [SerializeField] private GameObject telaDeBloqueio;
+    [SerializeField] private TMP_InputField inputSenBloqueio;
     private bool ativo = false;
+    private bool ehSenha = true;
 
     public void AbreBloco(){
         if(!ativo){
@@ -29,5 +34,24 @@ public class TextoComputador : MonoBehaviour
             this.resposta.SetActive(true);
             this.block.SetActive(false);
         }
+    }
+
+    public void VerificaSenhaUser(string senha){
+        if(senha == "1234"){
+            this.areaDeTrabalho.SetActive(true);
+            this.telaDeBloqueio.SetActive(false);
+        }
+    }
+
+    public void MudaTipoTexto(){
+        if(ehSenha){
+            this.inputSenBloqueio.contentType = TMP_InputField.ContentType.Standard;
+            ehSenha = !ehSenha;
+        } 
+        else {
+            this.inputSenBloqueio.contentType = TMP_InputField.ContentType.Password;
+            ehSenha = !ehSenha;
+        }
+        inputSenBloqueio.Select();
     }
 }
