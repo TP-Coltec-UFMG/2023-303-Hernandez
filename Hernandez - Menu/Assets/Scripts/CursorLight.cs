cb638f1352131aabfe2ddf50e188dc99931056d0
Izabela Fonseca <2021951493@teiacoltec.org>
2023-09-05 00:01:37 -0300

    E QUE HAJA LUZ

---
+++ Hernandez - Menu/Assets/Scripts/CursorLight.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
+        this.transform.position = mousePosition;
+    }
+}
